b2629526049422b59c0afd06d94f9008af78c942
dianamar34 <santos24garcia@unitec.edu>
2017-02-03 17:36:09 -0600

    Se crean arraylist para poder asignar los datos del reporte

--- P2_Lab3_DianaSantos/src/p2_lab3_dianasantos/Ventas.java
+++ P2_Lab3_DianaSantos/src/p2_lab3_dianasantos/Ventas.java
@@ -1,11 +1,15 @@
 
 package p2_lab3_dianasantos;
 
+import java.util.ArrayList;
+
 
 public class Ventas extends Carros{
-
+    ArrayList<Carros> carros = new ArrayList();
+    ArrayList<Empleados> empleado = new ArrayList();
     public Ventas() {
     }
+    
     
     
 }
